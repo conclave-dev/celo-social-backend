57a8b474257f877b678221347f32a341ba3f3984
Khoa Phan <khoaphan89@gmail.com>
2020-02-04 21:42:14 -0500

    Add and register routes/service

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,11 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"github.com/stella-zone/celo-social-backend/routes/profile"
+	"github.com/stella-zone/celo-social-backend/routes/service"
 )
 
 // SetUpRoutes initiates the setup process for all routes
 func SetUpRoutes(router *mux.Router) {
 	profile.AddRoutes(router)
+	service.AddRoutes(router)
 }
